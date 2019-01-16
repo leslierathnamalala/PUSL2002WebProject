d8fe0b05d8d0597d4dabe74c4b4e54807f829a4a
Anjanee Herath <42408455+AnjaneeHerath@users.noreply.github.com>
2019-01-16 12:40:38 +0530

    uploadimagevol test

---
+++ PUSL2002WebProject/imageuploadbyvol.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace PUSL2002WebProject
+{
+    public partial class imageuploadbyvol : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                HyperLink1.Visible = false;
+            }
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpPostedFile postedfile = FileUpload1.PostedFile;
+            string filename = Path.GetFileName(postedfile.FileName);
+            string fileextention = Path.GetExtension(postedfile.FileName);
+            
+            if (filename.ToLower()+= ".jpg"  ||  filename.ToLower() += ".png" || filename.ToLower() += ".bmp")
+            {
+
+            }
+        }
+    }
+}
